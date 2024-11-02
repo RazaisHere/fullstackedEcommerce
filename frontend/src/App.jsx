49ea4a3fa57a2637f42dd76025051a8c182cb20a
Ali Raza <info213243@gmail.com>
2024-11-02 14:37:27 +0500

    smoothScroll

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,10 +5,10 @@ import Collection from "./pages/Collection";
 import Contact from "./pages/Contact";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-
+import { ReactLenis } from "lenis/react";
 import Product from "./components/Product";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Cart from "./pages/Cart";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
@@ -17,30 +17,31 @@ import Orders from "./pages/Orders";
 import Verify from "./pages/Verify";
 function App() {
   return (
-    <div
-      className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]"
-       style={{ fontFamily: "Poppins, sans-serif" }}
-    >
-      <ToastContainer/>
-      <Navbar />
-    
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/collection" element={<Collection />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/place-order" element={<PlaceOrder/>}/>
-        <Route path="/orders" element={<Orders/>}/>
-        <Route path="/verify" element={<Verify/>}/>
+    <ReactLenis root>
+      <div
+        className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]"
+        style={{ fontFamily: "Poppins, sans-serif" }}
+      >
+        <ToastContainer />
+        <Navbar />
 
-        <Route path="/product/:productId" element={<Product />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/collection" element={<Collection />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/place-order" element={<PlaceOrder />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/verify" element={<Verify />} />
 
-      </Routes>
-      <Footer/>
-    </div>
+          <Route path="/product/:productId" element={<Product />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+        <Footer />
+      </div>
+    </ReactLenis>
   );
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,14 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import { ShopContextProvider } from './context/ShopContext';
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { ShopContextProvider } from "./context/ShopContext";
+import "lenis/dist/lenis.css";
 
-
-createRoot(document.getElementById('root')).render(
-
-    <BrowserRouter>
-   <ShopContextProvider>
-   <App />
-   </ShopContextProvider>
-    </BrowserRouter>
-,
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <ShopContextProvider>
+      <App />
+    </ShopContextProvider>
+  </BrowserRouter>
+);
