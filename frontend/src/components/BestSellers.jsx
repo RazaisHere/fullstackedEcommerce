19e77c1d8d5f62ae6916b79f0269fbca3e13e589
Ali Raza <info213243@gmail.com>
2024-11-02 16:01:03 +0500

    fixed bestSeller

--- frontend/src/components/BestSellers.jsx
+++ frontend/src/components/BestSellers.jsx
@@ -25,7 +25,7 @@ function BestSellers() {
       </div>
 
       {/* Products */}
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6 reveal-up">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6 ">
         {bestseller.map((item) => (
           <ProductItems
             key={item._id}
